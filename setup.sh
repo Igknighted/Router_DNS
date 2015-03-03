34d20c4651daeecbb96d7080223458ab3c9bc509
Sam <sam.igknighted@gmail.com>
2015-03-03 17:52:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ -f /etc/debian_version ] && grep -q -i ubuntu /etc/issue; then
 	echo Running ubuntu install script.
-	wget -qO- https://raw.githubusercontent.com/Igknighted/Router_DNS/master/setup_ubuntu.sh | bash
+	wget https://raw.githubusercontent.com/Igknighted/Router_DNS/master/setup_ubuntu.sh | bash setup_ubuntu.sh
 elif [ -f /etc/redhat-release ]; then
 	echo Running RHEL install script.
-	wget -qO- https://raw.githubusercontent.com/Igknighted/Router_DNS/master/setup_rhel.sh | bash
+	wget -https://raw.githubusercontent.com/Igknighted/Router_DNS/master/setup_rhel.sh | bash setup_rhel.sh
 fi
